187d1b3942fa72ff0c57d6bda053dd27c3ed98b2
Benjamin Vial <b.vial@qmul.ac.uk>
2020-02-17 00:07:45 +0000

    specified iqsharp kernel for prebuild

--- tutorials/prebuild.sh
+++ tutorials/prebuild.sh
@@ -2,4 +2,5 @@
 echo "Prebuilding: Q# samples..."
 
 dotnet build
-jupyter nbconvert *.ipynb --execute --stdout --to markdown  --allow-errors  --ExecutePreprocessor.timeout=120
+jupyter nbconvert *qsharp.ipynb --execute --stdout --to markdown --allow-errors  --ExecutePreprocessor.kernel_name=iqsharp --ExecutePreprocessor.timeout=120
+jupyter nbconvert *python.ipynb --execute --stdout --to markdown --allow-errors --ExecutePreprocessor.timeout=120
