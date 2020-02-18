3488e52994493e0abdce433b199fc2a2960da33f
Benjamin Vial <b.vial@qmul.ac.uk>
2020-02-18 22:12:50 +0000

    html conversion

--- tutorials/prebuild.sh
+++ tutorials/prebuild.sh
@@ -4,3 +4,7 @@ echo "Prebuilding: Q# samples..."
 dotnet build
 jupyter nbconvert *qsharp.ipynb --execute --stdout --to markdown --allow-errors  --ExecutePreprocessor.kernel_name=iqsharp --ExecutePreprocessor.timeout=120
 jupyter nbconvert *python.ipynb --execute --stdout --to markdown --allow-errors --ExecutePreprocessor.timeout=120
+
+
+jupyter nbconvert *.ipynb
+jupyter nbconvert *.ipynb --to slides
